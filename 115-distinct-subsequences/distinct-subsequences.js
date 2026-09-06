a19f580a46faa02a61969d6d2e73b2daa465abcd
Sampath Vinay Ram Vuppala <vinnuabivuppala@gmail.com>
2026-09-06 23:34:36 +0530

    Time: 61 ms (42.46%) | Memory: 82.4 MB (46.83%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.js
+++ 115-distinct-subsequences/distinct-subsequences.js
@@ -6,17 +6,17 @@
 var numDistinct = function(s, t) {
     let n=s.length;
     let m=t.length;
-    let dp=Array.from({length:n} , ()=>Array(m).fill(-1));
+    let dp=Array.from({length:n+1} , ()=>Array(m+1).fill(-1));
     function count_occ(s1,s2,idx1,idx2){
-        if(idx2<0) return 1;
-        if(idx1<0) return 0;
+        if(idx2===0) return 1;
+        if(idx1===0) return 0;
         if(dp[idx1][idx2] !==-1) return dp[idx1][idx2];
-        if(s1[idx1]===s2[idx2]){
+        if(s1[idx1-1]===s2[idx2-1]){
             dp[idx1][idx2]=count_occ(s1,s2,idx1-1,idx2-1)+count_occ(s1,s2,idx1-1,idx2);
         }else{
             dp[idx1][idx2]=count_occ(s1,s2,idx1-1,idx2);
         }
         return dp[idx1][idx2];
     }
-    return count_occ(s,t,n-1,m-1);
+    return count_occ(s,t,n,m);
 };
