88b02d659bd3c18f39ea48f26f9326f42a3b11be
Sampath Vinay Ram Vuppala <vinnuabivuppala@gmail.com>
2026-09-07 02:00:36 +0530

    Time: 79 ms (53.91%) | Memory: 85.6 MB (80.87%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.js
+++ 435-non-overlapping-intervals/non-overlapping-intervals.js
@@ -4,15 +4,15 @@
  */
 var eraseOverlapIntervals = function(intervals) {
     let n=intervals.length;
-    let last_ele=-Infinity;
     let cnt=0;
     intervals.sort((a,b)=>a[1]-b[1]);
+    let last=-Infinity;
     for(let i=0;i<n;i++){
-        if(intervals[i][0] >= last_ele){
+        let start=intervals[i][0];
+        if(start >=last){
             cnt=cnt+1;
-            last_ele=intervals[i][1];
-        }
+            last=intervals[i][1];}
     }
-    let res=intervals.length-cnt;
+    let res=n-cnt;
     return res;
 };
