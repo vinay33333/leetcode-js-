e1456d1709e117bdf58c89c7756c22cfd6a85c89
Sampath Vinay Ram Vuppala <vinnuabivuppala@gmail.com>
2026-09-12 00:21:20 +0530

    Time: 4 ms (66.67%) | Memory: 56.8 MB (66.67%) - LeetSync

--- 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.js
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.js
@@ -12,7 +12,7 @@ var totalNumbers = function(digits) {
                     continue;
                 }
                 let req_num=(digits[i]*100) + (digits[j]*10) + (digits[k]*1)
-                if(req_num >=100 && req_num%2===0){
+                if(req_num >=100 && req_num < 999 && req_num%2===0){
                     res.add(req_num);
                 }
             }
