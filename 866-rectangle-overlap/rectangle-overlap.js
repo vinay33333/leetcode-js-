42315031c9a59855634d7961e113cd941bb30b98
Sampath Vinay Ram Vuppala <vinnuabivuppala@gmail.com>
2026-09-15 01:24:15 +0530

    Time: 0 ms (100.00%) | Memory: 53.5 MB (48.89%) - LeetSync

---
+++ 866-rectangle-overlap/rectangle-overlap.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} rec1
+ * @param {number[]} rec2
+ * @return {boolean}
+ */
+var isRectangleOverlap = function(rec1, rec2) {
+    let [x1,y1,x2,y2]=rec1;
+    let [a1,b1,a2,b2]=rec2;
+    if(b1>=y2 ||y1>=b2 || a1>=x2||x1>=a2){
+        return false;
+    }
+    return true;
+};
