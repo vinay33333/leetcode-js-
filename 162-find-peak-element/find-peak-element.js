af34633695225649618caf5d2cbc2cd33c82d9c5
Sampath Vinay Ram Vuppala <vinnuabivuppala@gmail.com>
2026-09-26 00:45:22 +0530

    Time: 0 ms (100.00%) | Memory: 53.8 MB (43.62%) - LeetSync

--- 162-find-peak-element/find-peak-element.js
+++ 162-find-peak-element/find-peak-element.js
@@ -4,10 +4,16 @@
  */
 var findPeakElement = function(nums) {
     let n=nums.length;
-    for(let i=0;i<n;i++){
-        let left=(i===0||nums[i-1]<nums[i]);
-        let right=(i===n-1 ||nums[i]>nums[i+1]);
-        if(left&&right) return i;
+    if(n===1) return 0;
+    if(nums[0] >nums[1]) return 0;
+    if(nums[n-1] >nums[n-2]) return n-1;
+    let l=1;
+    let h=n-2;
+    while(l<=h){
+        let m=Math.floor((l+h)/2);
+        if(nums[m] >nums[m-1] &&nums[m+1]<nums[m]) return m;
+        else if(nums[m] > nums[m-1]) l=m+1;
+        else h=m-1;
     }
     return -1;
 };
